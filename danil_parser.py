1cf4059bcbb6b951571a78596688a8fc33a5f69d
sbxx-dev <the.shibax.games.228@gmail.com>
2026-05-15 20:11:07 +0300

    danil_parser add comments

--- danil_parser.py
+++ danil_parser.py
@@ -19,4 +19,14 @@ sell_size_df = pd.read_parquet(filenames["sell_size"])
 df_3d = pd.concat([open_df, close_df, high_df, low_df, buy_size_df, sell_size_df], axis=1, keys=list(filenames.keys()))
 df_swap = df_3d.swaplevel(0, 1, axis=1).sort_index(axis=1) #result
 
+# данные будем передавать в pandas dataframe'ах с несколькими уровнями колонок
+# 0 уровень – название акции
+# 1 уровень – по параметру на название акции
+
+# примерный вид:
+
+# АКЦИЯ
+# buy_size close high low open sell_size
+# число    число число число число число
+
 print(df_swap)
